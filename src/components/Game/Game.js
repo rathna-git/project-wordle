d64c6fac9c659f0265f3eff6323149c0ce921883
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-09-25 11:24:18 -0700

    chore: cleanup

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import Form from '../Form/Form'
 import Guess from '../Guess/guess';
+import Keyboard from '../Keyboard/Keyboard';
 import WinBanner from '../WinBanner/WinBanner';
 import LoseBanner from '../LoseBanner/LoseBanner';
 
-// import { range } from '../../utils';
-// import { checkGuess } from '../../game-helpers';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+
 
 
 // Pick a random word on every pageload.
@@ -22,10 +22,6 @@ function Game() {
 
   const [inputs, setInputs] = React.useState([]);
   const [status, setStatus] = React.useState('running');
-  // const [noOfTries, setNoOfTries] = React.useState(0);
-  // const [end, setEnd] = React.useState(false)
-  
-  // let results=[];
 
 
   function handleGuessSubmit(guess){
@@ -42,67 +38,11 @@ function Game() {
     }
   }
 
-  // function checkStatus(guess){
-  //    const newStatus = checkGuess(guess, answer);
-  //    setStatus(newStatus);
-  //    setNoOfTries(noOfTries + 1);
-  // }
-  
-
-  // if(status.length > 1){
-  //     range(5).map((num) => (
-  //         results.push(status[num]['status'])
-  //     ));
-  // }
-
-
-//  console.log("guess results: ", results);
-//  console.log("No. of Tries: ", noOfTries);
-
-
-//  function checkGameStatus(results){
-
-  
-//   let gameStatus = false;
-
-  
-
-//   if(results.length === 0 || results.includes('incorrect') || results.includes('misplaced')){
-//       gameStatus = false;
-//   }else{
-//       gameStatus = true;
-//       setEnd(true);
-//   }
-
-//   console.log('inside func game status', gameStatus);
-
-//   if(noOfTries === NUM_OF_GUESSES_ALLOWED){
-//     setEnd(true);
-//     return(
-//       <div className="sad banner">
-//         <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
-//       </div>
-//     )
-//   } else {
-//     return(
-//       <>
-//         {gameStatus && 
-//         <div className="happy banner">
-//         <p>
-//           <strong>Congratulations!</strong> Got it in 
-//           <strong> {noOfTries} guesses</strong>.
-//         </p>
-//         </div>}
-//       </>
-//      )
-//   }
-        
-// }
-
   return (
   <>
       <Guess inputs={inputs} answer={answer}/>
-      <Form handleGuessSubmit = {handleGuessSubmit} gameStatus={status}/>   
+      <Form handleGuessSubmit = {handleGuessSubmit} gameStatus={status}/> 
+      <Keyboard inputs={inputs} answer={answer}/>  
       {status === 'won' && (
         <WinBanner numOfGuesses={inputs.length}/>
         )
